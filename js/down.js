192683fc2d872fc92d32733e4c098aeb37e7b84d
Manoj Kumar <vsrmanoj.99@gmail.com>
2020-08-12 19:34:35 +0530

    fifth commit

--- js/down.js
+++ js/down.js
@@ -1,3 +1,14 @@
 AOS.init({
   duration: 1000
 });
+
+
+var jumboHeight = $('.jumbotron').outerHeight();
+function parallax(){
+    var scrolled = $(window).scrollTop();
+    $('.video-backgrorund').css('height', (jumboHeight-scrolled) + 'px');
+}
+
+$(window).scroll(function(e){
+    parallax();
+});
